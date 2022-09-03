7f30a073be195094ec502744f4d38510541fc46d
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2022-09-03 18:37:42 +0200

    Barricade base class

---
+++ Hungover/Assets/Source/Interactables/Barricade.cs
@@ -0,0 +1,53 @@
+namespace Hungover.Interactables
+{
+    public abstract class Barricade : Interactable
+    {
+        #region protected Fields
+
+        protected bool isLocked = true;
+        protected bool isOpen = false;
+
+        #endregion
+
+        #region Abstract Methods
+
+        protected abstract bool ConditionsToUnlockAreMet(Interactor interactor);
+
+        #endregion
+
+        #region Virtual Methods
+
+        protected virtual void Open(){}
+        protected virtual void Close(){}
+
+        #endregion
+
+        #region Interactable Methods
+
+        public override void OnInteract(Interactor interactor)
+        {
+            if (ConditionsToUnlockAreMet(interactor))
+            {
+                isLocked = false;
+            }
+
+            if (isLocked)
+            {
+                return;
+            }
+
+            if (isOpen)
+            {
+                Close();
+                isOpen = false;
+            }
+            else
+            {
+                Open();
+                isOpen = true;
+            }
+        }
+
+        #endregion
+    }
+}
