247f57d8e4a7df27e8e09b2275071f2387c53f39
Scott-Burnett <thebreakfastfactoryyay@gmail.com>
2022-09-05 18:55:55 +0200

    Add fridge prefab

---
+++ Hungover/Assets/Source/Interactables/Barricades/BreakableBarricades/Icicle.cs
@@ -0,0 +1,9 @@
+using Hungover.Interactables.Carryables;
+
+namespace Hungover.Interactables.Barricades.BreakableBarricades
+{
+    public class Icicle : BreakableBarrricade<Blowtorch>
+    {
+        
+    }
+}
