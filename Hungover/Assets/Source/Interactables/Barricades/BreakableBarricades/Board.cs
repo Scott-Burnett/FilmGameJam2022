6c8ca035c68eead654104f14a8d88912d7f1ac51
Roarke Angelbright <roarke.angelbright@storytoys.com>
2022-09-09 20:55:26 +0200

    Final visual effects

--- Hungover/Assets/Source/Interactables/Barricades/BreakableBarricades/Board.cs
+++ Hungover/Assets/Source/Interactables/Barricades/BreakableBarricades/Board.cs
@@ -1,13 +1,21 @@
 using Hungover.Interactables.Carryables;
+using UnityEngine;
 
 namespace Hungover.Interactables.Barricades.BreakableBarricades
 {
     public class Board : BreakableBarrricade<Crowbar>
     {
+        [SerializeField] ParticleSystem breakEffect;
         protected override void Initialise()
         {
             base.Initialise();
             DisableInteractSound();
+        }
+
+        protected override void OnUnlock(Interactor interactor)
+        {
+            breakEffect?.Play();
+            Destroy(this.gameObject);
         }
     }
 }
